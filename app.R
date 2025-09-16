c266f4cdc218712dea6c4481e1f36a06618352e7
zach.krogstad <zach.krogstad@activedecisionsupport.com>
2025-09-16 14:27:10 -0400

    try another logging

--- app.R
+++ app.R
@@ -2,13 +2,15 @@ library(shiny)
 library(useself)
 
 generate_story <- function(noun, verb, adjective, adverb) {
-  glue::glue(
-    "
+  story <-
+    glue::glue(
+      "
     Once upon a time, there was a {adjective} {noun} who loved to
     {verb} {adverb}. It was the funniest thing ever!
   "
-  )
-  cat("hi", file = stderr())
+    )
+  cat(story, file = stderr())
+  story
 }
 
 ui <- fluidPage(
